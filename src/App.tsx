a4fd6305c222ddeece33b7eeb851076fa32d3785
Nam Nguyen <nam.quoine@gmail.com>
2018-09-14 13:45:27 +0700

    Add CommentCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import styled from 'styled-components'
 
-import logo from './logo.svg';
+const AppWrapper = styled.div`
+  display: flex;
+`
 
-class App extends React.Component {
+export default class App extends React.Component {
   public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <AppWrapper>
+      <h1>Hi there</h1>
+    </AppWrapper>
   }
 }
-
-export default App;
